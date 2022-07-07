10f8c8288b9da37b41a17b17575b09ce69af7c47
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-07-07 18:18:04 +0530

    Updates

---
+++ precache-manifest.8c18400c872e98b23f14c3762bba4a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919c35bb63939eca7c4fe119148ad1ad",
+    "url": "/Tourism/index.html"
+  },
+  {
+    "revision": "9437a1a63dfdf59aa354",
+    "url": "/Tourism/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Tourism/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tourism/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9437a1a63dfdf59aa354",
+    "url": "/Tourism/static/js/main.0a2eed5f.chunk.js"
+  },
+  {
+    "revision": "935762899e0af6d32ebc",
+    "url": "/Tourism/static/js/runtime-main.bfa92496.js"
+  }
+]);
